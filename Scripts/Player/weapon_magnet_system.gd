65de178e78573df681d92a08fedd80db99ba08a7
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-05-21 23:25:37 +0100

    Added a cooldown timer for pulling after using the push function.

--- Scripts/Player/weapon_magnet_system.gd
+++ Scripts/Player/weapon_magnet_system.gd
@@ -1,21 +1,26 @@
 extends RayCast3D
 
 @onready var magnet_area = $MagnetArea
+@onready var pull_cooldown = $PullCooldown
 @onready var default = $Default
 
 #Handles pulling magnetic objects to the player
 func pull() -> void:
+	if !pull_cooldown.is_stopped():
+		return
 	magnet_area.set_global_position(get_magnet_point())
 	var mag_pos = magnet_area.get_global_position()
 	for body in magnet_area.get_overlapping_bodies():
 		if body.is_in_group("Magnetic"):
 			body.pull(mag_pos,10*sqrt(mag_pos.distance_to(body.get_position())))
 
+#Handles pushing weapons away from the magnet point
 func push() -> void:
 	magnet_area.set_global_position(get_magnet_point())
 	for body in magnet_area.get_overlapping_bodies():
 		if body.is_in_group("Magnetic"):
-			body.push(get_global_position(),10*sqrt(get_global_position().distance_to(body.get_position())))
+			body.push(get_global_position(),20)
+	pull_cooldown.start(1)
 
 #Gets the point of the magnatisms origin
 func get_magnet_point() -> Vector3:
